50a1583d75022e03149849c683ba7bca9e3a8123
WacuMbugua <58767172+WacuMbugua@users.noreply.github.com>
2020-01-07 06:07:18 +0300

    Router

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-import Vue from "vue";
-import App from "./App.vue";
 
-Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+// 1. Define route components.
+// These can be imported from other files
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+
+// Define some routes
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+//Create the router instance and pass the `routes` option
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+// 4. Create and mount the root instance.
+const app = new Vue({
+  router
+}).$mount('#app')
+
+
+
