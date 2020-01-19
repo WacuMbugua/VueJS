86dc5ca252c4bce84e1d8c8c118487df32a0e5e8
WacuMbugua <58767172+WacuMbugua@users.noreply.github.com>
2020-01-19 22:51:11 +0300

    new app

--- App.vue
+++ App.vue
@@ -1,17 +1,25 @@
 <template>
-<div id="app">
-   <h1>App component</h1>
-</div>
-<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+  </div>
+</template>
 
 <script>
-  import User1 from './user1.vue';
-  import User2 from './user2.vue';
-export default {
-    components:{
-        'app-user1':User1,
-        'app-user2':User2
-    }
-}
+import message from './components/HelloWorld.vue';
 
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+
+
+    }
+  }
+  
 </script>
+<style>
+
+</style>
